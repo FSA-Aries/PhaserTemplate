4db115a617b15c59ddcc63a5a40e16a5d850369a
Brandon Fox <spiritriderproboards@yahoo.com>
2021-01-29 15:27:08 -0500

    player class fully split off from game scene

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -37,58 +37,15 @@ export default class GameScene extends Phaser.Scene {
     const worldLayer = map.createLayer("Walls", tileSet, 0, 0);
 
     this.player = this.createPlayer();
-
-    this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   ///// UPDATE /////
-  update() {
-    this.player.body.setVelocity(0);
-
-    const { left, right, up, down } = this.cursors;
-
-    if (left.isDown) {
-      this.player.setVelocityX(-150);
-      this.player.anims.play("left", true);
-    } else if (right.isDown) {
-      this.player.setVelocityX(150);
-      this.player.anims.play("right", true);
-    } else {
-      this.player.setVelocityX(0);
-      this.player.anims.play("turn");
-    }
-
-    if (up.isDown) {
-      this.player.setVelocityY(-150);
-    } else if (down.isDown) {
-      this.player.setVelocityY(150);
-    }
-  }
+  update() {}
 
   ///// HELPER FUNCTIONS /////
 
   // PLAYER ANIMATION
   createPlayer() {
-    const player = new Player(this, 400, 375);
-    // const player = this.physics.add.sprite(400, 375, PLAYER_KEY);
-    player.setCollideWorldBounds(true);
-    this.anims.create({
-      key: "left",
-      frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 0, end: 3 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-    this.anims.create({
-      key: "turn",
-      frames: [{ key: PLAYER_KEY, frame: 4 }],
-      frameRate: 10,
-    });
-    this.anims.create({
-      key: "right",
-      frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 5, end: 8 }),
-      frameRate: 10,
-      repeat: -1,
-    });
-    return player;
+    return new Player(this, 400, 375);
   }
 }

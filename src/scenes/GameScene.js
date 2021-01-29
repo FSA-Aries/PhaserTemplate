1af2a980c219efa52a9d6a3949a449aa322279c4
Brandon Fox <spiritriderproboards@yahoo.com>
2021-01-29 14:38:54 -0500

    created new player class

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,18 +1,19 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
+import Player from "../entities/Player";
 
 // PLAYER
-const PLAYER = 'assets/characters/Player.png';
-const PLAYER_KEY = 'player';
+const PLAYER = "assets/characters/Player.png";
+const PLAYER_KEY = "player";
 // TILESET
-const TILESET = 'assets/tilesets/TileSet.png';
-const TILESET_KEY = 'tileSet';
+const TILESET = "assets/tilesets/TileSet.png";
+const TILESET_KEY = "tileSet";
 // TILEMAP
-const TILEMAP = 'assets/tilesets/TiledMap.json';
-const TILEMAP_KEY = 'main';
+const TILEMAP = "assets/tilesets/TiledMap.json";
+const TILEMAP_KEY = "main";
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
-    super('game-scene');
+    super("game-scene");
     this.player = undefined;
     this.cursors = undefined;
   }
@@ -31,9 +32,9 @@ export default class GameScene extends Phaser.Scene {
   ///// CREATE /////
   create() {
     let map = this.make.tilemap({ key: TILEMAP_KEY });
-    let tileSet = map.addTilesetImage('TiledSet', TILESET_KEY);
-    const belowLayer = map.createLayer('Ground', tileSet, 0, 0);
-    const worldLayer = map.createLayer('Walls', tileSet, 0, 0);
+    let tileSet = map.addTilesetImage("TiledSet", TILESET_KEY);
+    const belowLayer = map.createLayer("Ground", tileSet, 0, 0);
+    const worldLayer = map.createLayer("Walls", tileSet, 0, 0);
 
     this.player = this.createPlayer();
 
@@ -44,20 +45,22 @@ export default class GameScene extends Phaser.Scene {
   update() {
     this.player.body.setVelocity(0);
 
-    if (this.cursors.left.isDown) {
+    const { left, right, up, down } = this.cursors;
+
+    if (left.isDown) {
       this.player.setVelocityX(-150);
-      this.player.anims.play('left', true);
-    } else if (this.cursors.right.isDown) {
+      this.player.anims.play("left", true);
+    } else if (right.isDown) {
       this.player.setVelocityX(150);
-      this.player.anims.play('right', true);
+      this.player.anims.play("right", true);
     } else {
       this.player.setVelocityX(0);
-      this.player.anims.play('turn');
+      this.player.anims.play("turn");
     }
 
-    if (this.cursors.up.isDown) {
+    if (up.isDown) {
       this.player.setVelocityY(-150);
-    } else if (this.cursors.down.isDown) {
+    } else if (down.isDown) {
       this.player.setVelocityY(150);
     }
   }
@@ -66,21 +69,22 @@ export default class GameScene extends Phaser.Scene {
 
   // PLAYER ANIMATION
   createPlayer() {
-    const player = this.physics.add.sprite(400, 375, PLAYER_KEY);
+    const player = new Player(this, 400, 375);
+    // const player = this.physics.add.sprite(400, 375, PLAYER_KEY);
     player.setCollideWorldBounds(true);
     this.anims.create({
-      key: 'left',
+      key: "left",
       frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 0, end: 3 }),
       frameRate: 10,
       repeat: -1,
     });
     this.anims.create({
-      key: 'turn',
+      key: "turn",
       frames: [{ key: PLAYER_KEY, frame: 4 }],
       frameRate: 10,
     });
     this.anims.create({
-      key: 'right',
+      key: "right",
       frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 5, end: 8 }),
       frameRate: 10,
       repeat: -1,

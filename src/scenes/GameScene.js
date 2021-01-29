df3160df863684fa41beda169791b6a21fe8e67d
Morgan <morgan.hu738@gmail.com>
2021-01-29 13:46:17 -0500

    improved player movement and included animations

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -36,6 +36,8 @@ export default class GameScene extends Phaser.Scene {
     const worldLayer = map.createLayer('Walls', tileSet, 0, 0);
 
     this.player = this.createPlayer();
+    this.player.setTexture(PLAYER_KEY, 1);
+
 
     this.cursors = this.input.keyboard.createCursorKeys();
   }
@@ -43,23 +45,67 @@ export default class GameScene extends Phaser.Scene {
   ///// UPDATE /////
   update() {
     this.player.body.setVelocity(0);
+    const prevVelocity = this.player.body.velocity.clone();
+
 
     if (this.cursors.left.isDown) {
       this.player.setVelocityX(-150);
-      this.player.anims.play('left', true);
     } else if (this.cursors.right.isDown) {
       this.player.setVelocityX(150);
-      this.player.anims.play('right', true);
-    } else if (this.cursors.up.isDown) {
+    }
+    if (this.cursors.up.isDown) {
       this.player.setVelocityY(-150);
-      this.player.anims.play('up', true);
     } else if (this.cursors.down.isDown) {
       this.player.setVelocityY(150);
-      this.player.anims.play('down', true);
+    }
+
+    if (this.cursors.left.isDown) {
+      this.player.anims.play("left", true);
+    } else if (this.cursors.right.isDown) {
+      this.player.anims.play("right", true);
+    } else if (this.cursors.up.isDown) {
+      this.player.anims.play("up", true);
+    } else if (this.cursors.down.isDown) {
+      this.player.anims.play("down", true);
     } else {
+      this.player.anims.stop();
+
+      if (prevVelocity.x < 0) this.player.setTexture(PLAYER_KEY, 4);
+      else if (prevVelocity.x > 0) this.player.setTexture(PLAYER_KEY, 8);
+      else if (prevVelocity.y < 0) this.player.setTexture(PLAYER_KEY, 10);
+      else if (prevVelocity.y > 0) this.player.setTexture(PLAYER_KEY, 1);
+    }
+
+
+
+
+    /*     if (this.cursors.left.isDown && this.cursors.up.isDown) {
+          this.player.setVelocityX(-150);
+          this.player.setVelocityY(-150);
+          this.player.anims.play('left', true);
+        }
+        if (this.cursors.left.isDown && this.cursors.down.isDown) {
+          this.player.setVelocityX(-150);
+          this.player.setVelocityY(150);
+          this.player.anims.play('left', true);
+        }
+        if (this.cursors.right.isDown && this.cursors.up.isDown) {
+          this.player.setVelocityX(150);
+          this.player.setVelocityY(-150);
+          this.player.anims.play('right', true);
+        }
+        if (this.cursors.right.isDown && this.cursors.down.isDown) {
+          this.player.setVelocityX(150);
+          this.player.setVelocityY(150);
+          this.player.anims.play('right', true);
+        } */
+
+    /* if (this.player.body.velocity.x === 0 && this.player.body.velocity.y === 0) {
       this.player.setVelocityX(0);
       this.player.anims.play('turn');
-    }
+    } */
+
+
   }
 
   ///// HELPER FUNCTIONS /////
@@ -72,7 +118,7 @@ export default class GameScene extends Phaser.Scene {
       key: 'left',
       frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 3, end: 5 }),
       frameRate: 10,
-      repeat: -1,
+      //repeat: -1,
     });
     this.anims.create({
       key: 'turn',
@@ -83,17 +129,19 @@ export default class GameScene extends Phaser.Scene {
       key: 'right',
       frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 6, end: 8 }),
       frameRate: 10,
-      repeat: -1,
+      //repeat: -1,
     });
     this.anims.create({
       key: 'up',
       frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 9, end: 11 }),
       frameRate: 10,
+      //repeat: -1
     });
     this.anims.create({
       key: 'down',
       frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 0, end: 2 }),
       frameRate: 10,
+      //repeat: -1
     });
     return player;
   }

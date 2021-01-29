c92d8fe85d86ecb3998892118bb5fa687a289017
Kelvin Lin <kelvindlin97@gmail.com>
2021-01-29 12:31:08 -0500

    Added placeholder enemy sprite and updated GameScene with enemy movement

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,19 +1,23 @@
-import Phaser from 'phaser';
+import Phaser from "phaser";
 
 // PLAYER
-const PLAYER = 'assets/characters/Player.png';
-const PLAYER_KEY = 'player';
+const PLAYER = "assets/characters/Player.png";
+const PLAYER_KEY = "player";
 // TILESET
-const TILESET = 'assets/tilesets/TileSet.png';
-const TILESET_KEY = 'tileSet';
+const TILESET = "assets/tilesets/TileSet.png";
+const TILESET_KEY = "tileSet";
 // TILEMAP
-const TILEMAP = 'assets/tilesets/TiledMap.json';
-const TILEMAP_KEY = 'main';
+const TILEMAP = "assets/tilesets/TiledMap.json";
+const TILEMAP_KEY = "main";
+//ENEMY
+const ENEMY = "assets/characters/Enemy.png";
+const ENEMY_KEY = "enemy";
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
-    super('game-scene');
+    super("game-scene");
     this.player = undefined;
+    this.enemy = undefined;
     this.cursors = undefined;
   }
 
@@ -21,6 +25,11 @@ export default class GameScene extends Phaser.Scene {
   preload() {
     this.load.image(TILESET_KEY, TILESET);
     this.load.tilemapTiledJSON(TILEMAP_KEY, TILEMAP);
+
+    this.load.spritesheet(ENEMY_KEY, ENEMY, {
+      frameWidth: 32,
+      frameHeight: 48,
+    });
 
     this.load.spritesheet(PLAYER_KEY, PLAYER, {
       frameWidth: 32,
@@ -31,9 +40,9 @@ export default class GameScene extends Phaser.Scene {
   ///// CREATE /////
   create() {
     let map = this.make.tilemap({ key: TILEMAP_KEY });
-    let tileSet = map.addTilesetImage('TiledSet', TILESET_KEY);
-    const belowLayer = map.createLayer('Ground', tileSet, 0, 0);
-    const worldLayer = map.createLayer('Walls', tileSet, 0, 0);
+    let tileSet = map.addTilesetImage("TiledSet", TILESET_KEY);
+    const belowLayer = map.createLayer("Ground", tileSet, 0, 0);
+    const worldLayer = map.createLayer("Walls", tileSet, 0, 0);
 
     this.player = this.createPlayer();
 
@@ -46,13 +55,13 @@ export default class GameScene extends Phaser.Scene {
 
     if (this.cursors.left.isDown) {
       this.player.setVelocityX(-150);
-      this.player.anims.play('left', true);
+      this.player.anims.play("left", true);
     } else if (this.cursors.right.isDown) {
       this.player.setVelocityX(150);
-      this.player.anims.play('right', true);
+      this.player.anims.play("right", true);
     } else {
       this.player.setVelocityX(0);
-      this.player.anims.play('turn');
+      this.player.anims.play("turn");
     }
 
     if (this.cursors.up.isDown) {
@@ -69,18 +78,40 @@ export default class GameScene extends Phaser.Scene {
     const player = this.physics.add.sprite(400, 375, PLAYER_KEY);
     player.setCollideWorldBounds(true);
     this.anims.create({
-      key: 'left',
+      key: "left",
       frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 0, end: 3 }),
       frameRate: 10,
       repeat: -1,
     });
     this.anims.create({
-      key: 'turn',
+      key: "turn",
       frames: [{ key: PLAYER_KEY, frame: 4 }],
       frameRate: 10,
     });
     this.anims.create({
-      key: 'right',
+      key: "right",
+      frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    return player;
+  }
+  createEnemy() {
+    const player = this.physics.add.sprite(400, 375, PLAYER_KEY);
+    player.setCollideWorldBounds(true);
+    this.anims.create({
+      key: "left",
+      frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.anims.create({
+      key: "turn",
+      frames: [{ key: PLAYER_KEY, frame: 4 }],
+      frameRate: 10,
+    });
+    this.anims.create({
+      key: "right",
       frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 5, end: 8 }),
       frameRate: 10,
       repeat: -1,

677fb425b6d5dc530868029edc69cf42b0039121
Kelvin Lin <kelvindlin97@gmail.com>
2021-02-01 12:11:38 -0500

    Refactored enemy key and enemy url

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,19 +1,23 @@
 const assets = {};
 
 // ARSENAL
-assets.BULLET_KEY = 'bullet';
-assets.BULLET_URL = 'assets/arsenal/Bullet.png';
-assets.RETICLE_KEY = 'reticle';
-assets.RETICLE_URL = 'assets/arsenal/Reticle.png';
+assets.BULLET_KEY = "bullet";
+assets.BULLET_URL = "assets/arsenal/Bullet.png";
+assets.RETICLE_KEY = "reticle";
+assets.RETICLE_URL = "assets/arsenal/Reticle.png";
 
 // CHARACTERS
-assets.PLAYER_KEY = 'player';
-assets.PLAYER_URL = 'assets/characters/PokemonDude.png';
+assets.PLAYER_KEY = "player";
+assets.PLAYER_URL = "assets/characters/PokemonDude.png";
+
+//ENEMIES
+assets.ENEMY_KEY = "enemy";
+assets.ENEMY_URL = "assets/characters/Enemy/zombies.png";
 
 // TILESET
-assets.TILEMAP_KEY = 'tileMap';
-assets.TILEMAP_URL = 'assets/tilesets/TiledMap.json';
-assets.TILESET_KEY = 'tileSet';
-assets.TILESET_URL = 'assets/tilesets/TileSet.png';
+assets.TILEMAP_KEY = "tileMap";
+assets.TILEMAP_URL = "assets/tilesets/TiledMap.json";
+assets.TILESET_KEY = "tileSet";
+assets.TILESET_URL = "assets/tilesets/TileSet.png";
 
 export default assets;

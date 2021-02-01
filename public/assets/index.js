8855201a3ffaa12dc209141b8b85eeffccdb302f
Juan Velazquez <juandvelaz@gmail.com>
2021-01-31 21:25:50 -0600

    Updated Reticle and Bullet images

--- public/assets/index.js
+++ public/assets/index.js
@@ -5,9 +5,11 @@ assets.BULLET_KEY = 'bullet';
 assets.BULLET_URL = 'assets/arsenal/Bullet.png';
 assets.RETICLE_KEY = 'reticle';
 assets.RETICLE_URL = 'assets/arsenal/Reticle.png';
+
 // CHARACTERS
 assets.PLAYER_KEY = 'player';
 assets.PLAYER_URL = 'assets/characters/PokemonDude.png';
+
 // TILESET
 assets.TILEMAP_KEY = 'tileMap';
 assets.TILEMAP_URL = 'assets/tilesets/TiledMap.json';

2e72946c6e6df84fbc474f9fbc62615abbea1aba
Morgan <morgan.hu738@gmail.com>
2021-02-01 12:23:50 -0500

    bugFix(reticle now follows mouse)

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,21 +1,22 @@
-import Phaser from 'phaser';
-import Bullet from '../classes/Bullet'
+// //ENEMY
+// const ENEMY = "assets/characters/Enemy/zombies.png";
+// const ENEMY_KEY = "enemy";
 
-// PLAYER
-const PLAYER = 'assets/characters/pokemonDude.png';
-const PLAYER_KEY = 'player';
-// TILESET
-const TILESET = 'assets/tilesets/TileSet.png';
-const TILESET_KEY = 'tileSet';
-// TILEMAP
-const TILEMAP = 'assets/tilesets/TiledMap.json';
-const TILEMAP_KEY = 'main';
+import Phaser from "phaser";
+import Enemy from "./Enemy.js";
+import Player from "../classes/Player";
+import Bullet from "../classes/Bullet";
+import assets from "../../public/assets";
+
+import { config } from "../main";
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
-    super('game-scene');
+    super("game-scene");
     this.player = undefined;
+    this.enemy = undefined;
     this.cursors = undefined;
+    this.game = undefined;
     this.reticle = undefined;
   }
 
@@ -23,152 +24,115 @@ export default class GameScene extends Phaser.Scene {
 
   ///// PRELOAD /////
   preload() {
-    this.load.image('bullet', 'assets/characters/Bullet.png')
-    this.load.image('reticle', 'assets/characters/reticle.png')
+    this.load.image(assets.BULLET_KEY, assets.BULLET_URL);
+    this.load.image(assets.RETICLE_KEY, assets.RETICLE_URL);
+    this.load.image(assets.TILESET_KEY, assets.TILESET_URL);
+    this.load.tilemapTiledJSON(assets.TILEMAP_KEY, assets.TILEMAP_URL);
 
-    this.load.image(TILESET_KEY, TILESET);
-    this.load.tilemapTiledJSON(TILEMAP_KEY, TILEMAP);
-
-    this.load.spritesheet(PLAYER_KEY, PLAYER, {
+    this.load.spritesheet(assets.ENEMY_KEY, assets.ENEMY_URL, {
+      frameWidth: 30,
+      frameHeight: 62,
+    });
+    this.load.spritesheet(assets.PLAYER_KEY, assets.PLAYER_URL, {
       frameWidth: 50,
       frameHeight: 69,
     });
+    // const player = this.physics.add.sprite(400, 375, assets.PLAYER_KEY);
   }
 
   ///// CREATE /////
   create() {
-
-    let playerBullets = this.physics.add.group({ classType: Bullet, runChildUpdate: true })
-    this.reticle = this.physics.add.sprite(0, 0, 'reticle')
-    this.reticle.setDisplaySize(25, 25).setCollideWorldBounds(true);
-
-
-
-
-    let map = this.make.tilemap({ key: TILEMAP_KEY });
-    let tileSet = map.addTilesetImage('TiledSet', TILESET_KEY);
-    const belowLayer = map.createLayer('Ground', tileSet, 0, 0);
-    const worldLayer = map.createLayer('Walls', tileSet, 0, 0);
+    let map = this.make.tilemap({ key: assets.TILEMAP_KEY });
+    let tileSet = map.addTilesetImage("TiledSet", assets.TILESET_KEY);
+    map.createLayer("Ground", tileSet, 0, 0);
+    map.createLayer("Walls", tileSet, 0, 0);
 
     this.player = this.createPlayer();
-    this.player.setTexture(PLAYER_KEY, 1);
-
+    this.player.setTexture(assets.PLAYER_KEY, 1);
+    this.enemy = this.createEnemy();
 
     this.cursors = this.input.keyboard.createCursorKeys();
+    let playerBullets = this.physics.add.group({
+      classType: Bullet,
+      runChildUpdate: true,
+    });
+    this.reticle = this.physics.add.sprite(0, 0, assets.RETICLE_KEY);
+    this.reticle.setDisplaySize(25, 25).setCollideWorldBounds(true);
 
-    ///////////////////
+    this.input.on(
+      "pointerdown",
+      function () {
+        if (this.player.active === false) return;
 
+        // Get bullet from bullets group
+        let bullet = playerBullets.get().setActive(true).setVisible(true);
 
+        if (bullet) {
+          bullet.fire(this.player, this.reticle);
+          //this.physics.add.collider(enemy, bullet, enemyHitCallback);
+        }
+      },
+      this
+    );
+    this.setupFollowupCameraOn(this.player);
 
+    this.input.on(
+      "pointermove",
+      function (pointer) {
+        //console.log(this.input.mousePointer.x)
+        const transformedPoint = this.cameras.main.getWorldPoint(pointer.x, pointer.y);
 
-    this.input.on('pointerdown', function (pointer, time, lastFired) {
-      if (this.player.active === false)
-        return;
+        this.reticle.x = transformedPoint.x;
+        this.reticle.y = transformedPoint.y;
+        //console.log('if')
 
-      // Get bullet from bullets group
-      let bullet = playerBullets.get().setActive(true).setVisible(true);
+        //console.log(this.reticle)
 
-      if (bullet) {
-        bullet.fire(this.player, this.reticle);
-        //this.physics.add.collider(enemy, bullet, enemyHitCallback);
-      }
-    }, this);
-
-    //////////////////////
+        //console.log(pointer.movementY)
+        //this.player.rotation = angle;
+      },
+      this
+    );
   }
 
-  ///// UPDATE /////
-  update() {
-    this.player.body.setVelocity(0);
-    const prevVelocity = this.player.body.velocity.clone();
+  //       this
+  //     );
+  //   }
 
-
-    if (this.cursors.left.isDown) {
-      this.player.setVelocityX(-150);
-    } else if (this.cursors.right.isDown) {
-      this.player.setVelocityX(150);
-    }
-    if (this.cursors.up.isDown) {
-      this.player.setVelocityY(-150);
-    } else if (this.cursors.down.isDown) {
-      this.player.setVelocityY(150);
-    }
-
-    if (this.cursors.left.isDown) {
-      this.player.anims.play("left", true);
-    } else if (this.cursors.right.isDown) {
-      this.player.anims.play("right", true);
-    } else if (this.cursors.up.isDown) {
-      this.player.anims.play("up", true);
-    } else if (this.cursors.down.isDown) {
-      this.player.anims.play("down", true);
-    } else {
-      this.player.anims.stop();
-
-      if (prevVelocity.x < 0) this.player.setTexture(PLAYER_KEY, 4);
-      else if (prevVelocity.x > 0) this.player.setTexture(PLAYER_KEY, 8);
-      else if (prevVelocity.y < 0) this.player.setTexture(PLAYER_KEY, 10);
-      else if (prevVelocity.y > 0) this.player.setTexture(PLAYER_KEY, 1);
-    }
-
-
-    let angle = 0;
-    this.input.on('pointermove', function (pointer) {
-      angle = Phaser.Math.Angle.BetweenPoints(this.player, pointer);
-
-      //console.log(this.input.mousePointer.x)
-
-      this.reticle.x = this.input.x
-      this.reticle.y = this.input.y
-      //console.log('if')
-
-
-      //console.log(this.reticle)
-
-
-      //console.log(pointer.movementY)
-      //this.player.rotation = angle;
-    }, this);
-
-
-  }
+  update() { }
 
   ///// HELPER FUNCTIONS /////
 
   // PLAYER ANIMATION
   createPlayer() {
-    const player = this.physics.add.sprite(400, 375, PLAYER_KEY);
-    player.setCollideWorldBounds(true);
-    this.anims.create({
-      key: 'left',
-      frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 3, end: 5 }),
-      frameRate: 10,
-      //repeat: -1,
-    });
-    this.anims.create({
-      key: 'turn',
-      frames: [{ key: PLAYER_KEY, frame: 1 }],
-      frameRate: 10,
-    });
-    this.anims.create({
-      key: 'right',
-      frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 6, end: 8 }),
-      frameRate: 10,
-      //repeat: -1,
-    });
-    this.anims.create({
-      key: 'up',
-      frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 9, end: 11 }),
-      frameRate: 10,
-      //repeat: -1
-    });
-    this.anims.create({
-      key: 'down',
-      frames: this.anims.generateFrameNumbers(PLAYER_KEY, { start: 0, end: 2 }),
-      frameRate: 10,
-      //repeat: -1
-    });
-    return player;
+    //const player = this.physics.add.sprite(400, 375, assets.PLAYER_KEY);
+
+    return new Player(this, 400, 375);
+  }
+
+  setupFollowupCameraOn(player) {
+    this.physics.world.setBounds(
+      0,
+      0,
+      config.width + config.mapOffset,
+      config.height
+    );
+
+    this.cameras.main
+      .setBounds(0, 0, config.width + config.mapOffset, config.height)
+      .setZoom(1.5);
+    this.cameras.main.startFollow(player);
+  }
+  createEnemy() {
+    const randomizedPosition = Math.random() * 450;
+    return new Enemy(
+      this,
+      randomizedPosition,
+      randomizedPosition,
+      assets.ENEMY_KEY,
+      assets.ENEMY_URL,
+      this.player
+    );
   }
 }
 
